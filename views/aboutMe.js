4048d371064d37958f427b679098543fe35b2d39
Dayna Dev <honeyeyes17@icloud.com>
2024-08-06 16:46:34 -0500

    SPA review

---
+++ views/aboutMe.js
@@ -0,0 +1,11 @@
+import { html } from "html-literal";
+
+export default () => html`
+  <section id="bio">
+    <h2>Vivamus ac justo eu nisi</h2>
+    <img src="assets/img/dog-with-flower.jpg" alt="me" />
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu</p>
+    <p>Quisque at hendrerit libero, eget interdum lectus. Etiam facilisis</p>
+    <p>Etiam egestas consectetur gravida. Nulla mollis suscipit</p>
+  </section>
+`;
